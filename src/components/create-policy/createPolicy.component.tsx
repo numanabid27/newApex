5aa0e0fd4d14f8286bcafa0d07e48fc9ab0ff562
Muhammad Ahmad <contactmuhammadahmad@gmail.com>
2024-11-19 00:48:09 +0500

    Build error resolved

--- src/components/create-policy/createPolicy.component.tsx
+++ src/components/create-policy/createPolicy.component.tsx
@@ -482,7 +482,7 @@ export default function CreatePolicy(id: any) {
           <Box>
             <FormGroup sx={style.customCheckBoxFlex}>
               {CHECK_BOXES.map((item, index)=>(
-                <FormControlLabel control={<Checkbox />} label={item} sx={style.customCheckBox} />
+                <FormControlLabel key={index} control={<Checkbox />} label={item} sx={style.customCheckBox} />
               ))}
             </FormGroup>
           </Box>
